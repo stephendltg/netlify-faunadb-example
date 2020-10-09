99fdc0761f76851b702cd5856429598cce1b60a2
stephendltg <s.deletang@laposte.net>
2020-10-09 19:21:50 +0200

    do

--- functions/get.js
+++ functions/get.js
@@ -1,11 +1,17 @@
 /* Import faunaDB sdk */
 let fetch = require("node-fetch")
 
+function getId(urlPath) {
+  return urlPath.match(/([^\/]*)\/*$/)[0]
+}
+
 exports.handler = (event, context) => {
   
   console.log(event.path)
+  console.log( getId(event.path))
+  console.log( JSON.parse(event.body) )
   
-  return fetch("http://94.76.218.170:8080/wp-json", {
+  return fetch("http://94.76.218.170:8080/wp-json/wp/v2/token", {
     headers: {
     'Content-Type': 'application/json',
     'Authorization': 'Basic ' + Buffer.from('epyo:epyois100%MAGIC', "utf8").toString("base64")
